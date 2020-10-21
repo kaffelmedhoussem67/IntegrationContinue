fffce59fcc0610531a431439299fc556c9f4115c
houssembaazoug <houssem.baazoug@esprit.tn>
2020-10-21 23:06:21 +0100

    “m”

--- src/main/java/tn/esprit/spring/repository/EntrepriseRepository.java
+++ src/main/java/tn/esprit/spring/repository/EntrepriseRepository.java
@@ -1,5 +1,7 @@
 package tn.esprit.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import tn.esprit.spring.entities.Employe;
@@ -7,5 +9,6 @@ import tn.esprit.spring.entities.Entreprise;
 
 public interface EntrepriseRepository extends CrudRepository<Entreprise, Integer>  {
 	
-	
+	public List<Entreprise> findAllEntrepriseyEmployeJPQL(int entid);
+	public List<Entreprise> getAllEmployeByMission(int missionId);
 }
--- src/main/java/tn/esprit/spring/services/IEmployeService.java
+++ src/main/java/tn/esprit/spring/services/IEmployeService.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.services;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Entreprise;
@@ -10,12 +12,13 @@ import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.entities.Timesheet;
 
 
+@Service
 public interface IEmployeService {
 	
 
 	
 	
-	
+	//comment
 
 	
 }
