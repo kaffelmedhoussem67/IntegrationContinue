e007b178d2f0e0d97df39ec2425512184b0282df
FeresDebagi <feres.debagi@gmail.com>
2020-10-30 19:07:11 +0000

    feres2

---
+++ src/test/java/tn/esprit/spring/TimesheetTest.java
@@ -0,0 +1,41 @@
+package tn.esprit.spring;
+
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date; 
+import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.entities.Timesheet;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.services.TimesheetServiceImpl;
+
+
+public TimesheetTest {
+	
+	
+	/*TimesheetServiceImpl TimesheetService=new TimesheetServiceImpl();
+	Timesheet Timesheet = new Timesheet();
+	Contrat contrat = new Contrat();
+	Mission mission = new Mission();
+	Date dateDebut;
+	Date dateFin;
+	int TimesheetId,depId;
+	@Test
+	public void testAjoutContrat() {
+		assertNotNull(TimesheetService.ajouterContrat(contrat));
+	}
+	@Test
+	public void testAjoutTimesheet() {
+		assertNotNull(TimesheetService.ajouterTimesheet(Timesheet));
+	}
+	@Test
+	public void testAjoutTimesheet() {
+		assertNotNull(TimesheetService.getTimesheetsByMissionAndDate(Timesheet,mission,dateDebut,dateFin));
+	}
+	
+	@Test
+	public void testaffecterTimesheetADepartement() {
+		assertNotNull(TimesheetService.affecterTimesheetADepartement(TimesheetId,depId));
+	}*/
+	
+}
