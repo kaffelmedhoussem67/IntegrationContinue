b70b2cc3728a219790a0a3344e4812f1c406d50f
aminayadi <amin.ayadi@esprit.tn>
2020-11-03 10:56:11 +0100

    amin service

--- src/main/java/tn/esprit/spring/services/IMissionService.java
+++ src/main/java/tn/esprit/spring/services/IMissionService.java
@@ -7,7 +7,8 @@ import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Mission;
 
 public interface IMissionService {
-	
+	public int ajouterMission(Mission mission);
+	public void affecterMissionADepartement(int missionId, int depId);
 	public List<Mission> findAllMissionByEmployeJPQL(int employeId);
 	
 }
